3a84eec1ca0cf1436acd6f40682159dac9ed8f9b
cha2hyun <cha2hyun.dev@gmail.com>
2022-11-16 02:43:35 +0900

    feat(footer): 로고 그라디언트 색상 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,8 +9,10 @@ export default function Footer() {
         <div className='border-t border-slate-200 py-6 md:flex md:items-center md:justify-between md:py-8'>
           {/* Copyrights note */}
           <div className='h6 mr-4 text-slate-500'>
-            <div className='h3 md:h4 mb-2 bg-gradient-to-r from-sky-500 to-blue-800 bg-clip-text text-transparent'>
-              낭만스키
+            <div className='h3 md:h4 mb-2 '>
+              <span className='bg-gradient-to-r from-sky-500 to-blue-800 bg-clip-text text-transparent'>
+                낭만스키
+              </span>
             </div>
           </div>
           <div className='mr-4 mt-4 text-xs text-gray-500 text-transparent md:mt-0 md:text-sm'>
